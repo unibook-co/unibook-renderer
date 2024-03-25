2d4e59c3a1be6121a9752449f78e3647a3555a28
혀느현스 <hyuns@hyuns.dev>
2024-03-25 15:37:24 +0900

    feat: 라이브러리 업데이트

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import ReactDOM from "react-dom/client";
-import "katex/dist/katex.min.css";
-import "prismjs/themes/prism.css";
-import "./libs/styles/styles.css";
-import "./style.css";
+import ReactDOM from 'react-dom/client';
+import 'katex/dist/katex.min.css';
+import 'prismjs/themes/prism.css';
+import './libs/style.css';
 
-import { UniBookRenderer } from "./libs";
-import { Code } from "./libs/plugins/code";
-import { Equation } from "./libs/plugins/equation";
-import { pageExample } from "./page";
+import { UniBookRenderer } from './libs';
+import { Code } from './libs/plugins/code';
+import { Equation } from './libs/plugins/equation';
+import { pageExample } from './page';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <>
-      <div className="m-auto w-fit">
-        <UniBookRenderer
-          page={pageExample}
-          components={{
-            Code,
-            Equation,
-          }}
-        />
-      </div>
-    </>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <>
+            <div className="m-auto w-fit">
+                <UniBookRenderer
+                    page={pageExample}
+                    components={{
+                        Code,
+                        Equation,
+                    }}
+                />
+            </div>
+        </>
+    </React.StrictMode>
 );
