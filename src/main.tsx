c7219496c419036738694aeee65d2ddfe6263534
혀느현스 <hyuns@hyuns.dev>
2024-03-25 15:37:50 +0900

    feat: 불필요한 코드 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -5,23 +5,10 @@ import 'katex/dist/katex.min.css';
 import 'prismjs/themes/prism.css';
 import './libs/style.css';
 
-import { UniBookRenderer } from './libs';
-import { Code } from './libs/plugins/code';
-import { Equation } from './libs/plugins/equation';
-import { pageExample } from './page';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <>
-            <div className="m-auto w-fit">
-                <UniBookRenderer
-                    page={pageExample}
-                    components={{
-                        Code,
-                        Equation,
-                    }}
-                />
-            </div>
+            <div className="m-auto w-fit"></div>
         </>
     </React.StrictMode>
 );
